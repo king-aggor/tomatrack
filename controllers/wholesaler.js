037a958071fa9e80e43a370b89a35498f9d96403
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-07-19 17:24:18 +0000

    updated wholesaler purchased products page

--- controllers/wholesaler.js
+++ controllers/wholesaler.js
@@ -3,6 +3,7 @@ exports.getAvailableProducts = (req, res, next) => {
   res.render("wholesaler/available-products", {
     path: "/wholesaler/available-products",
     role: "wholesaler",
+    title: "Available Products",
   });
 };
 
@@ -10,14 +11,16 @@ exports.getAvailableProducts = (req, res, next) => {
 exports.getAllProducts = (req, res, next) => {
   res.render("wholesaler/all-products", {
     path: "/wholesaler/all-products",
-    role: "wholsaler",
+    role: "wholesaler",
+    title: "All Products",
   });
 };
 
-// get product purchased by wholesaler
+//  get product purchased by wholesaler
 exports.getPurchasedProducts = (req, res, next) => {
   res.render("wholesaler/purchased-products", {
     path: "/wholesaler/purchased-products",
-    role: "wholsaler",
+    role: "wholesaler",
+    title: "Purchased Products",
   });
 };
