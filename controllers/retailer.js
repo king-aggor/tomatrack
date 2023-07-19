360d290f302474585edfeab9c12f85339f3eff85
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-07-19 19:34:45 +0000

    created and rendered retailer purchased products

--- controllers/retailer.js
+++ controllers/retailer.js
@@ -1,7 +1,16 @@
 // get all retailer products
-exports.getRetailerAllProducts = (req, res, next) => {
+exports.getAllProducts = (req, res, next) => {
   res.render("retailer/all-products", {
     path: "/retailer/all-products",
+    role: "retailer",
+    title: "All Products",
+  });
+};
+
+// get retailer purchased products
+exports.getPurchasedProducts = (req, res, next) => {
+  res.render("retailer/purchased-products", {
+    path: "/retailer/purchased-products",
     role: "retailer",
     title: "All Products",
   });
--- routes/retailer.js
+++ routes/retailer.js
@@ -9,7 +9,11 @@ const express = require("express"); //importing express
 // use the Router function on express and assign to router
 const router = express.Router();
 
-router.get("/all-products", retailerController.getRetailerAllProducts);
+// path to retailer all products
+router.get("/all-products", retailerController.getAllProducts);
+
+// path to retailer purchased product
+router.get("/purchased-products", retailerController.getPurchasedProducts);
 
 // export router
 module.exports = router;
