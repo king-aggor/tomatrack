2ca9005428c2f8a61a040d7041099770c7ff7c52
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-09-28 18:55:15 +0000

    added distributor id, distributor name, update purchased to true to product when distributor is buying a product

---
+++ controllers/distributor.js
@@ -0,0 +1,119 @@
+// local modules
+const User = require("../models/user"); //importing User model
+const Product = require("../models/product");
+
+// let distributorID;
+// get all distributor's products
+exports.getAllProducts = (req, res, next) => {
+  const userId = req.params.userId;
+  User.findById(userId)
+    .then((user) => {
+      const userID = user._id;
+      Product.find({ "distributor.User": userID })
+        .then((products) => {
+          res.render("distributor/all-products", {
+            path: "/distributor/all-products",
+            role: "distributor",
+            title: "All Products",
+            prods: products,
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// get wholesaler products available for purchase
+exports.getAvailableProducts = (req, res, next) => {
+  const userId = req.params.userId;
+  User.findById(userId)
+    .then((user) => {
+      Product.find({
+        "wholesaler.User": { $exists: true },
+        "distributor.purchased": false,
+      })
+        .then((products) => {
+          res.render("distributor/available-products", {
+            path: "/distributor/available-products",
+            role: "distributor",
+            title: "Available Products",
+            prods: products,
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// post buy product
+exports.postBuyProduct = (req, res, next) => {
+  const userId = req.body.distributorid;
+  const prodId = req.body.batchNum;
+
+  User.findById(userId)
+    .then((user) => {
+      console.log(user.orgName);
+      Product.updateOne(
+        { batchNum: prodId },
+        {
+          $set: {
+            "distributor.User": userId,
+            "distributor.purchased": true,
+            "distributor.distributor_name": user.orgName,
+          },
+        }
+      ).then((product) => {
+        res.redirect(`available-products/${userId}`);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// get purchased products
+exports.getPurchasedProduct = (req, res, next) => {
+  User.findById(userId)
+    .then((user) => {
+      const userId = user._id;
+      Product.find({
+        "distributor.User": userId,
+        "distributor.purchased": true,
+      })
+        .then((products) => {
+          for (let wholesaler of products) {
+            User.find({ _id: wholesaler.wholesaler.User })
+              .then((wholesalers) => {
+                // console.log(wholesalers);
+                res.render("distributor/purchased-products", {
+                  path: "/distributor/purchased-products",
+                  role: "distributor",
+                  title: "Purchased Products",
+                  prods: products,
+                  user: user,
+                  wholesalers: wholesalers,
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
---
+++ routes/distributor.js
@@ -0,0 +1,29 @@
+// local modules
+const distributorController = require("../controllers/distributor");
+
+// thirdparty modules
+const express = require("express"); //importing express
+
+// use the Router function on express and assign to router
+const router = express.Router();
+
+// get path to distributor all products
+router.get("/all-products/:userId", distributorController.getAllProducts);
+
+// get path to available products
+router.get(
+  "/available-products/:userId",
+  distributorController.getAvailableProducts
+);
+
+// post path to buy product
+router.post("/buy-product", distributorController.postBuyProduct);
+
+// get path to purchased products
+router.get(
+  "/purchased-products/:userId",
+  distributorController.getPurchasedProduct
+);
+
+// export router
+module.exports = router;
