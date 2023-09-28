17700c19c4be62ef06924378bf5e8b818ef5846c
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-09-28 20:11:09 +0000

    created User model

--- models/user.js
+++ models/user.js
@@ -1,40 +1,9 @@
-// // array to store users
-// const users = [];
-
-// let userid;
-// // create and export User class
-// module.exports = class User {
-//   constructor(id, role, name, email, country, region, password) {
-//     id = userid;
-//     this.role = role;
-//     this.name = name;
-//     this.email = email;
-//     this.country = country;
-//     this.region = region;
-//     this.password = password;
-//   }
-
-//   //   function to save user
-//   save() {
-//     // userid = Math.floor(Math.random() * 100 + 1).toString();
-//     userid = 1;
-//     this.id = userid;
-//     users.push(this);
-//   }
-
-//   //   function to find user
-//   static findUser(id) {
-//     if ((id = 1)) {
-//       return users;
-//     }
-//   }
-// };
-
-// third-party models
+// third-party modules
 const mongoose = require("mongoose"); //importing mongoose
 
 const Schema = mongoose.Schema;
 
+// defining user schema
 const userSchema = new Schema({
   orgName: {
     type: String,
