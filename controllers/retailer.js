70fe197b9cce158d30fe45b2e859a2203baa5bb3
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-09-29 19:40:57 +0000

    rendered retailer available-products.ejs

--- controllers/retailer.js
+++ controllers/retailer.js
@@ -1,10 +1,57 @@
+// local  modules
+const User = require("../models/user"); //importing User model
+const Product = require("../models/product"); //importing Product model
 // get all retailer products
 exports.getAllProducts = (req, res, next) => {
-  res.render("retailer/all-products", {
-    path: "/retailer/all-products",
-    role: "retailer",
-    title: "All Products",
-  });
+  const userId = req.params.userId;
+  User.findById(userId)
+    .then((user) => {
+      const retailerId = user._id;
+      Product.find({ "retailer.User": retailerId })
+        .then((products) => {
+          // console.log(products);
+          res.render("retailer/all-products", {
+            path: "/retailer/all-products",
+            role: "retailer",
+            title: "All Products",
+            prods: products,
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getAvailableProducts = (req, res, next) => {
+  const userId = req.params.userId;
+  User.findById(userId)
+    .then((user) => {
+      Product.find({
+        "distributor.User": { $exists: true },
+        "retailer.purchased": false,
+      })
+        .then((products) => {
+          // console.log(products);
+          res.render("retailer/available-products", {
+            path: "/retailer/available-products",
+            role: "retailer",
+            title: "Available Products",
+            prods: products,
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // get retailer purchased products
