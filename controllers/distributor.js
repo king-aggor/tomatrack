21bab62ba409655b2e102f883fd30c09ae0cb25f
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-11-14 15:46:26 +0000

    created distributor controllers to decline and confirm order requests

--- controllers/distributor.js
+++ controllers/distributor.js
@@ -2,14 +2,20 @@
 const User = require("../models/user"); //importing User model
 const Product = require("../models/product");
 
-// let distributorID;
+// core modules
+const crypto = require("crypto");
+
+let distributorID;
 // get all distributor's products
 exports.getAllProducts = (req, res, next) => {
-  const userId = req.params.userId;
-  User.findById(userId)
+  distributorID = req.params.userId;
+  User.findById(distributorID)
     .then((user) => {
       const userID = user._id;
-      Product.find({ "distributor.User": userID })
+      Product.find({
+        "distributor.User": userID,
+        "distributor.orderConfirmed": true,
+      })
         .then((products) => {
           res.render("distributor/all-products", {
             path: "/distributor/all-products",
@@ -30,8 +36,8 @@ exports.getAllProducts = (req, res, next) => {
 
 // get wholesaler products available for purchase
 exports.getAvailableProducts = (req, res, next) => {
-  const userId = req.params.userId;
-  User.findById(userId)
+  distributorID = req.params.userId;
+  User.findById(distributorID)
     .then((user) => {
       Product.find({
         "wholesaler.orderConfirmed": true,
@@ -57,11 +63,154 @@ exports.getAvailableProducts = (req, res, next) => {
 
 // post buy product
 exports.postBuyProduct = (req, res, next) => {
-  const userId = req.body.distributorid;
   const batchNum = req.body.batchNum;
+  const userId = distributorID;
+  User.findById(userId)
+    .then((user) => {
+      Product.findOne({ batchNum: batchNum })
+        .then((product) => {
+          Product.updateOne(
+            { batchNum: batchNum },
+            {
+              $set: {
+                distributor: {
+                  User: userId,
+                  ordered: true,
+                  distributor_name: user.orgName,
+                  location: {
+                    country: user.country,
+                    region: user.region,
+                  },
+                  price: ((product.price * 20) / 100 + product.price).toFixed(
+                    2
+                  ),
+                  ordered: true,
+                  orderConfirmed: false,
+                },
+              },
+            }
+          )
+            .then((product) => {
+              res.redirect(`available-products/${userId}`);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-  console.log(userId);
-  // res.redirect(`available-products/${userId}`);
+// post cancel order
+exports.postCancelOrder = (req, res, next) => {
+  const batchNum = req.body.batchNum;
+  const userId = req.body.userId;
+
+  Product.updateOne(
+    { "distributor.User": userId, batchNum: batchNum },
+    {
+      $set: {
+        distributor: {
+          User: crypto.randomBytes(12).toString("hex"),
+          ordered: false,
+          orderConfirmed: false,
+        },
+      },
+    }
+  )
+    .then((product) => {
+      console.log(product);
+      res.redirect(`available-products/${userId}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// get purchase requests
+exports.getPurchaseRequests = (req, res, next) => {
+  distributorID = req.params.userId;
+  const userId = distributorID;
+  // console.log(userId);
+  User.findById(userId)
+    .then((user) => {
+      Product.find({
+        "distributor.User": userId,
+        "retailer.ordered": true,
+        "retailer.orderConfirmed": false,
+      })
+        .then((products) => {
+          // console.log(products);
+          res.render("distributor/purchase-requests", {
+            path: "/distributor/purchase-requests",
+            role: "distributor",
+            title: "Purchase Requests",
+            prods: products,
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// post decline order
+exports.postDeclineOrder = (req, res, next) => {
+  const batchNum = req.body.batchNum;
+  const userId = distributorID;
+  // console.log(batchNum, userId);
+
+  Product.updateOne(
+    { "distributor.User": userId, batchNum: batchNum },
+    {
+      $set: {
+        retailer: {
+          User: crypto.randomBytes(12).toString("hex"),
+          ordered: false,
+          orderConfirmed: false,
+        },
+      },
+    }
+  )
+    .then((product) => {
+      console.log(product);
+      res.redirect(`purchase-requests/${userId}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// post comfirm order
+exports.postConfirmOrder = (req, res, next) => {
+  const batchNum = req.body.batchNum;
+  const userId = distributorID;
+  // console.log(batchNum, userId);
+
+  Product.updateOne(
+    { batchNum: batchNum, "distributor.User": userId },
+    {
+      $set: {
+        "retailer.orderConfirmed": true,
+      },
+    }
+  )
+    .then((product) => {
+      console.log(product);
+      res.redirect(`/distributor/purchase-requests/${userId}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // get purchased products
@@ -72,7 +221,7 @@ exports.getPurchasedProduct = (req, res, next) => {
       const userId = user._id;
       Product.find({
         "distributor.User": userId,
-        "distributor.purchased": true,
+        "distributor.orderConfirmed": true,
       })
         .then((products) => {
           res.render("distributor/purchased-products", {
@@ -97,10 +246,10 @@ exports.getSoldProducts = (req, res, next) => {
   const userId = req.params.userId;
   User.findById(userId)
     .then((user) => {
-      const distributorId = user._id;
+      // const distributorId = user._id;
       Product.find({
-        "distributor.User": distributorId,
-        "retailer.purchased": true,
+        "distributor.User": userId,
+        "retailer.orderConfirmed": true,
       })
         .then((products) => {
           res.render("distributor/sold-products", {
