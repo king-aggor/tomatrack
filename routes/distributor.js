fa8749f05fee4e198d071f380420dcbb433b866e
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-11-14 15:49:01 +0000

    created distributor route paths to decline and confrim order requests

--- routes/distributor.js
+++ routes/distributor.js
@@ -19,11 +19,26 @@ router.get(
 // post path to buy product
 router.post("/buy-product", distributorController.postBuyProduct);
 
+// posr path to cancel oder
+router.post("/cancel-order", distributorController.postCancelOrder);
+
 // get path to purchased products
 router.get(
   "/purchased-products/:userId",
   distributorController.getPurchasedProduct
 );
+
+// get path to purchase requests
+router.get(
+  "/purchase-requests/:userId",
+  distributorController.getPurchaseRequests
+);
+
+// post path to decline requests
+router.post("/decline-order", distributorController.postDeclineOrder);
+
+// post path  to confirm order
+router.post("/confirm-order", distributorController.postConfirmOrder);
 
 // get path to sold products
 router.get("/sold-products/:userId", distributorController.getSoldProducts);
