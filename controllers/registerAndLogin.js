322cd94e0c95e643d55c3f3b4001baa8503c082e
king-aggor <109685452+king-aggor@users.noreply.github.com>
2023-11-15 15:42:55 +0000

    compare user email and password input match a user

--- controllers/registerAndLogin.js
+++ controllers/registerAndLogin.js
@@ -47,11 +47,43 @@ exports.getLogin = (req, res, nex) => {
 
 exports.postLogin = (req, res, next) => {
   //find a user whose email & password matches user login input
-  User.findOne({ email: req.body.email, password: req.body.password })
+  //   User.findOne({ email: req.body.email, password: req.body.password })
+  //     .then((user) => {
+  //       const userId = user._id.toString();
+  //       const role = user.roles.User.toLowerCase();
+  //       res.redirect(`${role}/all-products/${userId}`);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // find a user whoses email matches user email input
+  User.findOne({ email: req.body.email })
     .then((user) => {
-      const userId = user._id.toString();
-      const role = user.roles.User.toLowerCase();
-      res.redirect(`${role}/all-products/${userId}`);
+      // console.log user does not exist if email doesnt match any user in DB
+      if (!user) {
+        console.log("User does note exist");
+      }
+      //if user exists compare user password input with hashed password in DB
+      else if (user) {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((password) => {
+            // console.log email and password do not match if password input  does not match user hashed password
+            if (user && !password) {
+              console.log("Email and Password do not match");
+            }
+            // if user exist and user input password matches hashed password, redirect to the stakeholder's all-products page
+            if (user && password) {
+              console.log("Loged in");
+              const userId = user._id.toString();
+              const role = user.roles.User.toLowerCase();
+              res.redirect(`${role}/all-products/${userId}`);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     })
     .catch((err) => {
       console.log(err);
